3e85e88e31180474b665ecaa19be2afdd3bf9bee
Jessica Ma <gejessicama@gmail.com>
2018-01-23 17:49:24 -0800

    sync before i leave

--- howto.sh
+++ howto.sh
@@ -26,6 +26,6 @@ $ makeblastdb -in source_fasta_file -dbtype 'prot'
 $ blastp -num_threads 4 -outfmt '6 std qcovs ppos' -evalue 1e-5 -query query_file.fa -subject source_file.fa > output_file.blastp
 $ blastp -num_threads 4 -evalue 1e-5 -query /projects/spruceup/scratch/psitchensis/Q903/annotation/amp/sequences/uniprot-ncbi-filtered-10Jan18.fa -subject /projects/spruceup/scratch/psitchensis/Q903/annotation/amp/sequences/uniprot-ncbi-filtered-10Jan18.fa > uniprot-ncbi-filtered-10Jan18-self.blastp
 
-blastp -evalue 1e-5 -outfmt '6 qaccver saccver evalue length nident mismatch positive ppos' -query /projects/spruceup/scratch/psitchensis/Q903/annotation/amp/sequences/uniprot-ncbi-filtered-10Jan18.fa -subject /projects/spruceup/scratch/psitchensis/Q903/annotation/amp/sequences/uniprot-ncbi-filtered-10Jan18.fa > selfblastJan23.blastp
+blastp -evalue 1e-5 -outfmt '10 qaccver saccver evalue length nident mismatch positive ppos' -query /projects/spruceup/scratch/psitchensis/Q903/annotation/amp/sequences/uniprot-ncbi-filtered-10Jan18.fa -subject /projects/spruceup/scratch/psitchensis/Q903/annotation/amp/sequences/uniprot-ncbi-filtered-10Jan18.fa > selfblastJan23.blastp
 
 
